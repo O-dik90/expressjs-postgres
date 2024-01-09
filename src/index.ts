dee9407c1139871d6b427c2e08dfcdb9766a2481
O-dik90 <98860631+O-dik90@users.noreply.github.com>
2024-01-09 21:12:55 +0700

    Update index.ts
    update posting method

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ app.get("/api", async (req, res) => {
 
 app.post("/api/:id", async (req, res) => {
   var distance = req.params.id
+  const {rows} = await pool.query(`INSERT INTO distance (desc, distance) VALUES("test", ${distance}) `);
   res.send(`get id distance ${distance}`);
 });
 
