3b6756cf03e07284afdc1da7191754847475db07
O-dik90 <98860631+O-dik90@users.noreply.github.com>
2024-01-11 00:45:29 +0700

    Update index.ts
    udpate insert data

--- src/index.ts
+++ src/index.ts
@@ -35,10 +35,9 @@ app.post("/api/post", async (req, res) => {
 });
 
 app.post("/api/post1", async (req, res) => {
-  let data = [["testing", "5"]];
-  
-  const sql = `INSERT INTO dist(desc,distance) VALUES ?;`;
-  await pool.query(sql, [data], (err,result) => {
+ 
+  const sql = `INSERT INTO dist(desc,distance) VALUES(?, ?);`;
+  await pool.run(sql, ['test', '5'], (err,result) => {
     if (err) {
       res.status(400).send(err);
     } else {
