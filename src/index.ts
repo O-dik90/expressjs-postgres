1684ebb886ce4b3f22b4ed1d83c85ee41c0755e4
O-dik90 <98860631+O-dik90@users.noreply.github.com>
2024-01-11 01:13:04 +0700

    Update index.ts
    update insert

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ app.post("/api/post", async (req, res) => {
 
 app.post("/api/post1", async (req, res) => {
  
-  const sql = `INSERT INTO dist(id,desc,distance) VALUES(?,?,?)`;
+  const sql = `INSERT INTO dist(id,description,distance) VALUES(?,?,?)`;
   await pool.query(sql, [2,'test', '5'], (err,result) => {
     if (err) {
       res.status(404).send(err.message);
